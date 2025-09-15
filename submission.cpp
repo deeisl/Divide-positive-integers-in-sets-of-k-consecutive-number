064822e1a27fdf2260e19955fc4dd1e4ebf19b81
deeisl <73616627+deeisl@users.noreply.github.com>
2025-09-15 13:20:09 -0500

    Update submission.cpp

--- submission.cpp
+++ submission.cpp
@@ -11,7 +11,7 @@ bool canDivideIntoSets(std::vector<int>& nums, int k) {
     }
 
     int n = nums.size();
-    std::sort(nums.begin(), nums.end());
+    sort(nums.begin(), nums.end());
 
     for (int i = 0; i < n - k + 1; i++) {
         if (nums[i + k - 1] - nums[i] == k - 1) {
